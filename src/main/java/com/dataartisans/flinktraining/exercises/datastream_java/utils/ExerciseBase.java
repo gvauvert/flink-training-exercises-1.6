c8d4f991eab917916e5d3495f1cf86bfac8367cb
Guillaume Vauvert <gvauvert@axway.com>
2018-09-17 08:13:39 +0200

    Update ExerciseBase Paths

--- src/main/java/com/dataartisans/flinktraining/exercises/datastream_java/utils/ExerciseBase.java
+++ src/main/java/com/dataartisans/flinktraining/exercises/datastream_java/utils/ExerciseBase.java
@@ -28,8 +28,8 @@ public class ExerciseBase {
 	public static SinkFunction out = null;
 	public static int parallelism = 4;
 
-	public final static String pathToRideData = "/Users/david/stuff/flink-training/trainingData/nycTaxiRides.gz";
-	public final static String pathToFareData = "/Users/david/stuff/flink-training/trainingData/nycTaxiFares.gz";
+	public final static String pathToRideData = "E:\\DataSource\\New_York_City_Taxi_FF2018\\nycTaxiRides.gz";
+	public final static String pathToFareData = "E:\\DataSource\\New_York_City_Taxi_FF2018\\nycTaxiFares.gz";
 
 	public static SourceFunction<TaxiRide> rideSourceOrTest(SourceFunction<TaxiRide> source) {
 		if (rides == null) {
